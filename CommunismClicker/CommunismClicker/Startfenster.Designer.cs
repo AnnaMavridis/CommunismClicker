d00e9969eb33971d396b5a95ff2f2367e03a5223
Tom Stute <stute.tom@gmail.com>
2025-06-17 10:09:11 +0200

    Startfenster UI hinzugefügt

---
+++ CommunismClicker/CommunismClicker/Startfenster.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CommunismClicker
+{
+    partial class Startfenster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            spielStarten = new Button();
+            weiterSpielen = new Button();
+            spielVerlassen = new Button();
+            SuspendLayout();
+            // 
+            // spielStarten
+            // 
+            spielStarten.Location = new Point(158, 69);
+            spielStarten.Name = "spielStarten";
+            spielStarten.Size = new Size(126, 62);
+            spielStarten.TabIndex = 0;
+            spielStarten.Text = "Spiel Starten";
+            spielStarten.UseVisualStyleBackColor = true;
+            spielStarten.Click += spielStarten_Click;
+            // 
+            // weiterSpielen
+            // 
+            weiterSpielen.Location = new Point(320, 69);
+            weiterSpielen.Name = "weiterSpielen";
+            weiterSpielen.Size = new Size(153, 62);
+            weiterSpielen.TabIndex = 1;
+            weiterSpielen.Text = "weiter Spielen";
+            weiterSpielen.UseVisualStyleBackColor = true;
+            weiterSpielen.Click += weiterSpielen_Click;
+            // 
+            // spielVerlassen
+            // 
+            spielVerlassen.Location = new Point(509, 69);
+            spielVerlassen.Name = "spielVerlassen";
+            spielVerlassen.Size = new Size(135, 62);
+            spielVerlassen.TabIndex = 2;
+            spielVerlassen.Text = "Spiel Verlassen";
+            spielVerlassen.UseVisualStyleBackColor = true;
+            spielVerlassen.Click += spielVerlassen_Click;
+            // 
+            // Startfenster
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(spielVerlassen);
+            Controls.Add(weiterSpielen);
+            Controls.Add(spielStarten);
+            Name = "Startfenster";
+            Text = "Startfenster";
+            Load += Startfenster_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button spielStarten;
+        private Button weiterSpielen;
+        private Button spielVerlassen;
+    }
+}
---
+++ CommunismClicker/CommunismClicker/Startfenster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommunismClicker
+{
+    public partial class Startfenster : Form
+    {
+        public Startfenster()
+        {
+            InitializeComponent();
+        }
+
+        private void Startfenster_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void spielStarten_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void weiterSpielen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void spielVerlassen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
