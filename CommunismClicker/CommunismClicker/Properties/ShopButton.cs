9511e2bab46c6ca1c274652b65d37671733880d7
Iivo161 <iivo.p2@gso.schule.koeln>
2025-06-24 10:20:25 +0200

    ShopFenster erstellt + ShopButton Klasse erstellt

---
+++ CommunismClicker/CommunismClicker/Properties/ShopButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+
+namespace CommunismClicker
+{
+    internal class ShopButton
+    {
+        public Rectangle Area { get; private set; }
+        public string Text { get; private set; }
+        public bool Geklickt { get; set; }
+
+        public ShopButton(int x, int y, int width, int height, string text)
+        {
+            Area = new Rectangle(x, y, width, height);
+            Text = text;
+            Geklickt = false;
+        }
+
+        public void Draw(Graphics g)
+        {
+            // Hintergrundfarbe je nach Zustand
+            Color fillColor = Geklickt ? Color.DarkRed : Color.Red;
+
+            using (Brush brush = new SolidBrush(fillColor))
+            {
+                g.FillRectangle(brush, Area);
+            }
+
+            using (Brush textBrush = new SolidBrush(Color.White))
+            using (StringFormat sf = new StringFormat
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            })
+            using (Font font = new Font("Arial", 12))
+            {
+                g.DrawString(Text, font, textBrush, Area, sf);
+            }
+
+            // Optional: Rahmen zeichnen
+            using (Pen borderPen = new Pen(Color.Black, 2))
+            {
+                g.DrawRectangle(borderPen, Area);
+            }
+        }
+
+        public bool Contains(Point p)
+        {
+            return Area.Contains(p);
+        }
+    }
+}
+
---
+++ CommunismClicker/CommunismClicker/Properties/ShopFenster.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CommunismClicker
+{
+    public partial class ShopFenster : Form
+    {
+       
+
+        private List<ShopButton> rectangles = new List<ShopButton>();
+
+        public ShopFenster()
+        {
+            this.Text = "blub blub";
+            this.DoubleBuffered = true;
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            this.Paint += ShopFenster_Paint;
+            this.MouseClick += ShopFenster_MouseClick;
+
+            // Einzelne Upgrades hinzufügen
+            AddRectangle(50, 50, 150, 80, "1.Upgrade");
+            AddRectangle(250, 50, 150, 80, "2.Upgrade");
+            AddRectangle(150, 180, 200, 100, "3.Upgrade");
+
+           
+        }
+
+        private void ShopFenster_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void AddRectangle(int x, int y, int width, int height, string text)
+        {
+            rectangles.Add(new ShopButton(x, y, width, height, text));
+        }
+
+        private void ShopFenster_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var rect in rectangles)
+            {
+                rect.Draw(e.Graphics);
+            }
+        }
+
+        private void ShopFenster_MouseClick(object sender, MouseEventArgs e)
+        {
+            foreach (var rect in rectangles)
+            {
+                if (!rect.Geklickt && rect.Contains(e.Location))
+                {
+                    rect.Geklickt = true;
+                    MessageBox.Show($"Rechteck '{rect.Text}' wurde geklickt!");
+                    Invalidate(); // Neu zeichnen
+                    break; // Nur eines pro Klick
+                }
+            }
+        }
+    }
+
+}
+
+
