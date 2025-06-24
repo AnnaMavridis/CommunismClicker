46f89bc24ec2fd95d76047a96358a99a7d6d9070
Tom Stute <stute.tom@gmail.com>
2025-06-24 09:53:13 +0200

    Irgendwelche Änderungen

---
+++ CommunismClicker/CommunismClicker/Spielstand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunismClicker
+{
+    internal class Spielstand
+    {
+        public Int64 währung;
+        public int upgrade, level;
+        public bool finish;
+    }
+}
